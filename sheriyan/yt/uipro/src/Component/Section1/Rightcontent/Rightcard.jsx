f810b9e57f2908c6075d0b5c5b5907e58afb9793
jaintulya <tulya.jain.cg@gmail.com>
2026-03-27 13:41:29 +0530

    updayed commit

--- sheriyan/yt/uipro/src/Component/Section1/Rightcontent/Rightcard.jsx
+++ sheriyan/yt/uipro/src/Component/Section1/Rightcontent/Rightcard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Rightcard = () => {
   return (
-    <div className="h-full  w-2/5 bg-red-600 relative overflow-hidden rounded-4xl">
+    <div className="h-full  w-60 bg-red-600 relative overflow-hidden rounded-4xl">
       <img
         className="h-full w-full object-fill "
         src="https://images.unsplash.com/photo-1755519024827-fd05075a7200?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d29yayUyMHByb2ZpbGUlMjBwaWN8ZW58MHx8MHx8fDA%3D"
         alt=""
       />
       <div className="absolute top-0 left-0 h-full w-full p-6 flex flex-col justify-between">
-        <h2 className="bg-white rounded-full w-8 p-2 ">1</h2>
+        <h2 className="bg-white text-xl font-semibold flex justify-center items-center rounded-full h-10 w-10  ">1</h2>
         <div>
-          <p>
+          <p className=" text-lg leading-normal">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore
-            dignissimos, officia fugiat esse nemo praesentium.
+            dignissimos, official fugiat esse nemo praesentium.
           </p>
           <div>
             <button>Satisfied</button>
