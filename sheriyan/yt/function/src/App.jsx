9c3e100915e94749113479c9665389fe89c55d8a
jaintulya <tulya.jain.cg@gmail.com>
2026-04-13 22:57:56 +0530

    13/4/26

---
+++ sheriyan/yt/function/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const App = () => {
+  const wheel = (elem)=>{
+    console.log(elem.deltaY)
+  }
+  return (
+    <div onWheel={wheel}>
+      <div className="page1">1</div>
+<div className="page2">2</div>
+<div className="page3">3</div>
+
+    </div>
+  )
+}
+
+export default App
