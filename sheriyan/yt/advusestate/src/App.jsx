0f2fb2fb38043784716be1ece25d03132fd47c19
jaintulya <tulya.jain.cg@gmail.com>
2026-04-15 20:30:40 +0530

    Update App.jsx and index.css

--- sheriyan/yt/advusestate/src/App.jsx
+++ sheriyan/yt/advusestate/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  
+
+const [num, setNum]=useState({user:'manan',age:18})
+  const clickedBtn = ()=>{
+    const newNum = {...num}
+    newNum.user = "Aman"
+    newNum.age = '21'
+setNum(newNum)
+  }
+  const dblbtn = () => {
+    setNum(num)
+  }
+
   return (
     <div>
-      
+      <h1>{num.user} {num.age}</h1>
+      <button onDoubleClick={dblbtn} onClick={clickedBtn}>Click to Change </button>
+
     </div>
   )
 }
